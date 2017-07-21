72a9b01e842cee71bc46bdf1ce761d7cb3833ad0
Blaise Braye <blaisemail@gmail.com>
2017-07-21 13:32:36 +0200

    Remove unused code

--- Operations.Classification.WpfUi.Tests/Accounts/Wrapper.cs
+++ Operations.Classification.WpfUi.Tests/Accounts/Wrapper.cs
@@ -1,5 +1,10 @@
 ﻿namespace Operations.Classification.WpfUi.Tests.Accounts
 {
+    /// <summary>
+    /// this wrapper is used as a trick to fool the test explorer.
+    /// Without this mecanism, some tests could be written with the autofixture style
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Wrapper<T>
     {
         public T Value { get; }
@@ -13,12 +18,7 @@
         {
             return typeof(T).ToString();
         }
-
-        public static implicit operator Wrapper<T>(T val)
-        {
-            return new Wrapper<T>(val);
-        }
-
+        
         public static implicit operator T(Wrapper<T> wrapper)
         {
             return wrapper.Value;
