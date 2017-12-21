df9fa65355a2098d284ce98e682b0e3350fb03f6
Blaise Braye <blaisemail@gmail.com>
2017-12-21 15:20:31 +0100

    add roadmap

---
+++ roadmap.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1513858645748" ID="ID_1232753149" MODIFIED="1513859250774" TEXT="MyAccounts roadmap">
+<node CREATED="1513858655272" ID="ID_589008583" MODIFIED="1513859226816" POSITION="right" TEXT="Import Fortis/Sodexo operations from multiple accounts">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1513858679135" ID="ID_1363331672" MODIFIED="1513859228041" POSITION="right" TEXT="Vizualize multi account operations / Filter them">
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1513860083372" ID="ID_601387093" MODIFIED="1513860120365" POSITION="right" TEXT="I need a simpler view on my operations">
+<icon BUILTIN="full-3"/>
+<icon BUILTIN="hourglass"/>
+<node CREATED="1513859027508" ID="ID_94968342" MODIFIED="1513860124578" TEXT="I want to import categories for my operations"/>
+<node CREATED="1513858872085" ID="ID_1196168473" MODIFIED="1513860053942" TEXT="I want to cluster easily the operations by category"/>
+<node CREATED="1513858795093" ID="ID_1186343593" MODIFIED="1513860099492" TEXT="I want to vizualize fast  the most impacting categories"/>
+<node CREATED="1513859120883" ID="ID_1292804084" MODIFIED="1513859138512" TEXT="I want to view the operations of those categories easily"/>
+<node CREATED="1513860012405" ID="ID_1004282634" MODIFIED="1513860102100" TEXT="I want to discover categories by operation similarity/frequency detection,..."/>
+</node>
+<node CREATED="1513858738207" ID="ID_1948226246" MODIFIED="1513859241272" POSITION="right" TEXT="Import any type of operation">
+<icon BUILTIN="full-4"/>
+<icon BUILTIN="go"/>
+<node CREATED="1513858752334" ID="ID_786319921" MODIFIED="1513858770858" TEXT="Requires operation mapping definitions"/>
+<node CREATED="1513858979604" ID="ID_1549164084" MODIFIED="1513859002985" TEXT="Define operation parsing rules from application"/>
+</node>
+<node CREATED="1513859252162" ID="ID_211178810" MODIFIED="1513859279096" POSITION="right" TEXT="I want to interact from my mobile phone">
+<icon BUILTIN="full-5"/>
+<icon BUILTIN="stop"/>
+</node>
+</node>
+</map>
